36acd386d56d71a67d2d1454090761ebc1e07d16
Emily Port <54337762+emjapo@users.noreply.github.com>
2022-02-07 00:18:04 -0500

    Login (#5)
    * removed bad comment types
    
    * Started working on getting the login created
    
    * html for login added
    
    * worked on login styling
    
    * web login style done
    
    * mobile login style done?
    
    * input changed to type password
    
    * fixed centering on link
    
    * created page directory for finished page components
    
    * changed it to mobile first design
    
    * Reset password page added
    
    Co-authored-by: Gerald <57047729+Gerald-Wheaton@users.noreply.github.com>

--- charm-tracker/imports/ui/App.js
+++ charm-tracker/imports/ui/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import { UP_Collection_Access } from './../api/user_posts.js';
 import Title from './Title.js';
 import Login from './pages/Login.js';
+import ResetPassword from "./pages/ResetPassword.js";
 import PropTypes from 'prop-types';
 
 export default class App extends React.Component {
